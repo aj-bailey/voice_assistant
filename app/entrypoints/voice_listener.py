7e20c666fc25c9ed0732eba69af5c00566eac2c9
Adam Bailey <ab67319@gmail.com>
2023-12-28 11:49:04 -0700

    Feat: Add voice recorder

--- app/entrypoints/voice_listener.py
+++ app/entrypoints/voice_listener.py
@@ -3,21 +3,21 @@ from gtts import gTTS
 import speech_recognition as sr
 
 class VoiceListener():
-    @classmethod
-    def listen_for_trigger() -> bool:
-        recognizer = sr.Recognizer()
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
 
+    def listen_for_trigger(self) -> bool:
         with sr.Microphone() as source:
             print("Listening for 'Hey Raspberry'...")
-            recognizer.adjust_for_ambient_noise(source)
-            audio = recognizer.listen(source)
+            self.recognizer.adjust_for_ambient_noise(source)
+            audio = self.recognizer.listen(source)
 
         try:
-            spoken_text = recognizer.recognize_google(audio).lower()
+            spoken_text = self.recognizer.recognize_google(audio).lower()
 
             print(f"You said: {spoken_text}")
             if "hey raspberry" in spoken_text:
-                speech = gTTS(text="what bitch?", tld='com.au', lang='en')
+                speech = gTTS(text="Hi")
                 speech_file = "speech.mp3"
                 speech.save(speech_file)
                 os.system('afplay '+ speech_file)
@@ -26,10 +26,30 @@ class VoiceListener():
                 return False
         except sr.UnknownValueError:
             return False
-        except sr.RequestError as e:
+        except Exception as e:
             print(f"Error: {e}")
             return False
 
-    @classmethod
-    def record_phrase():
-        pass
+    def record_phrase(self):
+        with sr.Microphone() as source:
+            print("Recording next phrase...")
+
+            self.recognizer.adjust_for_ambient_noise(source)
+            audio = self.recognizer.listen(source, timeout=5)  # Record for up to 5 seconds
+
+        try:
+            spoken_text = self.recognizer.recognize_google(audio)
+            print(f"Recorded phrase: {spoken_text}")
+
+            return spoken_text
+        except sr.UnknownValueError:
+            print("Unable to understand the recorded phrase.")
+
+            speech = gTTS(text="I didn't catch that")
+            speech_file = "speech.mp3"
+            speech.save(speech_file)
+            os.system('afplay '+ speech_file)
+        except sr.RequestError as e:
+            print(f"Error: {e}")
+
+            return None
