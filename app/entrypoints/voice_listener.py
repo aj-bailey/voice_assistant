fa8259f774dbd1018186e61980c78d8720133447
Adam Bailey <ab67319@gmail.com>
2023-12-28 10:52:22 -0700

    Refactor: Update line indents

--- app/entrypoints/voice_listener.py
+++ app/entrypoints/voice_listener.py
@@ -2,30 +2,34 @@ import os
 from gtts import gTTS
 import speech_recognition as sr
 
-
 class VoiceListener():
-  def listen_for_trigger() -> bool:
-    recognizer = sr.Recognizer()
+    @classmethod
+    def listen_for_trigger() -> bool:
+        recognizer = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("Listening for 'Hey Raspberry'...")
-        recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source)
+        with sr.Microphone() as source:
+            print("Listening for 'Hey Raspberry'...")
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
 
-    try:
-        spoken_text = recognizer.recognize_google(audio).lower()
-        
-        print(f"You said: {spoken_text}")
-        if "hey raspberry" in spoken_text:
-            speech = gTTS(text="what bitch?", tld='com.au', lang='en')
-            speech_file = "speech.mp3"
-            speech.save(speech_file)
-            os.system('afplay '+ speech_file)
-            return True
-        else:
+        try:
+            spoken_text = recognizer.recognize_google(audio).lower()
+
+            print(f"You said: {spoken_text}")
+            if "hey raspberry" in spoken_text:
+                speech = gTTS(text="what bitch?", tld='com.au', lang='en')
+                speech_file = "speech.mp3"
+                speech.save(speech_file)
+                os.system('afplay '+ speech_file)
+                return True
+            else:
+                return False
+        except sr.UnknownValueError:
             return False
-    except sr.UnknownValueError:
-        return False
-    except sr.RequestError as e:
-        print(f"Error: {e}")
-        return False
+        except sr.RequestError as e:
+            print(f"Error: {e}")
+            return False
+
+    @classmethod
+    def record_phrase():
+        pass
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from app.entrypoints.voice_listener import VoiceListener
 
 def main():
-  while True:
-    if VoiceListener.listen_for_trigger():
-      pass
+    while True:
+        if VoiceListener.listen_for_trigger():
+            phrase = VoiceListener.record_phrase()
 
 if __name__ == "__main__":
-  main()
+    main()
