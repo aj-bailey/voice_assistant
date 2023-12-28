0fce2ebe16ec57618e5b660f42fdf9db9646b0c8
Adam Bailey <ab67319@gmail.com>
2023-12-28 11:48:18 -0700

    Refactor: Update main loop logic

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
+import os
+
+from app.adapters.open_ai_repository import OpenAIRepository
 from app.entrypoints.voice_listener import VoiceListener
+from dotenv import load_dotenv
+
+load_dotenv()
+OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
 
 def main():
-    while True:
-        if VoiceListener.listen_for_trigger():
-            phrase = VoiceListener.record_phrase()
+    vl = VoiceListener()
+    open_ai_repository = OpenAIRepository()
+    continue_listening = True
+
+    while continue_listening:
+        if vl.listen_for_trigger():
+            phrase_recorded = False
+
+            while not phrase_recorded:
+                phrase = vl.record_phrase()
+                
+                if phrase:
+                    if phrase == 'stop listening':
+                        phrase_recorded = True
+                    elif phrase == 'exit':
+                        phrase_recorded = True
+                        continue_listening = False
+                    else:
+                        response = open_ai_repository.query(phrase)
+                        print(response)
+                        phrase_recorded = True
+
+    print("Exiting...")
 
 if __name__ == "__main__":
     main()
