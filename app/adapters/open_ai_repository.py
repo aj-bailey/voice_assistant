c8ba19b272954f2904654caaca0437541a740f80
Adam Bailey <ab67319@gmail.com>
2023-12-28 20:41:43 -0700

    Feat: Add audio response

--- app/adapters/open_ai_repository.py
+++ app/adapters/open_ai_repository.py
@@ -12,3 +12,12 @@ class OpenAIRepository():
             ]
         )
         return completion.choices[0].message.content
+    
+    def audio_response(self, message):
+        response = self.client.audio.speech.create(
+            model="tts-1",
+            voice="alloy",
+            input=message,
+        )
+
+        response.stream_to_file("speech.mp3")
